3cf97e1ffa1e5247a07ee344aac7239f23744d28
kyoko K <kyoko-nr@users.noreply.github.com>
2025-10-01 21:31:15 +0900

    wip

--- src/glsl/advectVelocity.glsl
+++ src/glsl/advectVelocity.glsl
@@ -15,11 +15,13 @@ void main() {
   vec4 data = texture2D(uData, uv);
 
   vec2 backUv0 = uv - data.xy * uDeltaT * uTexelSize;
-  vec2 backUv = mirrorRepeatUv(backUv0, uTexelSize);
+  // vec2 backUv = mirrorRepeatUv(backUv0, uTexelSize);
 
-  vec2 advect0 = sampleBilinear4(uData, backUv, uTexelSize).xy;
-  vec2 advect1 = advect0 * uDissipation;
-  vec2 advect = applyReflectiveBoundary(uv, uTexelSize, advect1, 1.0);
+  // vec2 advect0 = sampleBilinear4(uData, backUv0, uTexelSize).xy;
+  // vec2 advect1 = advect0 * uDissipation;
+  // vec2 advect = applyReflectiveBoundary(uv, uTexelSize, advect1, 1.0);
+
+  vec2 advect = texture2D(uData, backUv0).xy * uDissipation;
 
   gl_FragColor = vec4(advect, data.zw);
 }
--- src/glsl/render2.glsl
+++ src/glsl/render2.glsl
@@ -10,19 +10,30 @@ const float PI = 3.14159265358979323846;
 
 // 計算結果をレンダリングする
 void main() {
+  // vec2 uv0 = gl_FragCoord.xy * uTextureSize;
+  // vec2 uv = vec2(uv0.x, 1.0 - uv0.y);
+
+  // // data.xyに速度、data.zに圧力、data.wに発散が入っている
+  // vec4 data = texture2D(uTexture, uv);
+
+  // float hueBase = fract(atan(data.y, data.x) * (1.0 / (2.0 * PI)));
+  // float tri = 1.0 - abs(hueBase * 2.0 - 1.0);
+  // float hue = tri * (1.0 / 6.0) + uTimeStep;
+
+  // float speed = length(data.xy);
+  // float sat = clamp(speed * 40.0, 0.3, 0.9);
+
+  // vec3 color = hsv2Rgb(hue, sat, 0.9);
+  // gl_FragColor = vec4(color, 1.0);
+
   vec2 uv0 = gl_FragCoord.xy * uTextureSize;
   vec2 uv = vec2(uv0.x, 1.0 - uv0.y);
+  vec2 velocity = texture2D(uTexture, uv).xy;
+  float len = smoothstep(0.0, 1.0, length(velocity));
+  velocity = velocity * 0.5 + 0.5;
 
-  // data.xyに速度、data.zに圧力、data.wに発散が入っている
-  vec4 data = texture2D(uTexture, uv);
+  vec3 color = vec3(velocity.x, velocity.y, 1.0);
+  color = mix(vec3(1.0), color, len);
 
-  float hueBase = fract(atan(data.y, data.x) * (1.0 / (2.0 * PI)));
-  float tri = 1.0 - abs(hueBase * 2.0 - 1.0);
-  float hue = tri * (1.0 / 6.0) + uTimeStep;
-
-  float speed = length(data.xy);
-  float sat = clamp(speed * 40.0, 0.3, 0.9);
-
-  vec3 color = hsv2Rgb(hue, sat, 0.9);
   gl_FragColor = vec4(color, 1.0);
 }
